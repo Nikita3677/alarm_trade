5f3babca7f02b0ac5235391bb0b6cc8a15c5b3d6
Никита Лебедев <nikitaleb36@gmail.com>
2023-05-25 15:10:26 +0300

    Правки PEP8

--- main.py
+++ main.py
@@ -8,11 +8,14 @@ load_dotenv()
 api_key = os.getenv('api_key')
 secret_key = os.getenv('secret_key')
 
+
 def main():
     client = Client(api_key=api_key, api_secret=secret_key)
     while True:
         try:
-            price_now = client.futures_coin_symbol_ticker(pair='ETHUSD')[0].get('price')
+            price_now = client.futures_coin_symbol_ticker(
+                pair='ETHUSD'
+            )[0].get('price')
             start_time = int((time.time()-3600)*1000)
             stop_time = int((time.time()-3480)*1000)
             price_hour_ago = client.futures_coin_index_price_klines(
@@ -20,7 +23,8 @@ def main():
                 startTime=str(start_time), endTime=str(stop_time)
             )[0][1]
             # считаем изменение цены
-            procent = (abs(float(price_hour_ago) - float(price_now)))/float(price_hour_ago)
+            diff = (abs(float(price_hour_ago) - float(price_now)))
+            procent = diff / float(price_hour_ago)
         except BinanceAPIException as error:
             print(error)
         else:
@@ -29,6 +33,7 @@ def main():
                 print('Подъем! Пора торговать!')
                 time.sleep(60)
             time.sleep(1)
+
 
 if __name__ == '__main__':
     main()
